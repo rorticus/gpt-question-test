d5e1472049167f2f8bfe319e03d42c78a64bf698
Rory Mulligan <rorticus@gmail.com>
2023-09-13 09:08:37 -0400

    adding inappropriate response

--- src/SlideShow.tsx
+++ src/SlideShow.tsx
@@ -14,17 +14,23 @@ export default function SlideShow({ slides }: SlideShowProps) {
 
   const data = slides.items[index];
 
+  if (!slides.is_appropriate) {
+    return <div>This question is inappropriate! Naughty naughty.</div>;
+  }
+
   return (
     <div>
       <div className="slide-container">
         <div className="slide-image">
-            {data.image ? <img src={data.image} /> : null}
-            {!data.image ? <span>image for "{data.keywords}"</span> : null}
+          {data.image ? <img src={data.image} /> : null}
+          {!data.image ? <span>image for "{data.keywords}"</span> : null}
         </div>
         <div className="slide-text">{data.text}</div>
       </div>
       <div className="slide-controls">
-        <div>{index + 1} / {slides.items.length}</div>
+        <div>
+          {index + 1} / {slides.items.length}
+        </div>
         <button
           disabled={index === 0}
           onClick={() => {
